a7bbeb4fbf473f9a390af4acd6a7eaa965fda913
AlexeyKrestinin <alexeykrestinin@gmail.com>
2017-02-27 22:52:37 +0300

    Initial Commit

---
+++ Program1Lesson1KrestininAV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Program1Lesson1KrestininAV
+//
+//  Created by Алексей Крестинин on 27.02.17.
+//  Copyright © 2017 Alexey Krestinin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
